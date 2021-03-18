4561af939d6fec670be440afdd0411a36443c52b
Andrea Esposito <andexpox@gmail.com>
2021-03-18 17:40:23 +0100

    end

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Chidedi nome utente
+var nomeUtente = prompt( 'Come ti chiami?' );
+console.log( nomeUtente );
 
 // Chiedi cognome utente
+var cognomeUtente = prompt( 'Quale è il tuo cognome?' );
+console.log( cognomeUtente );
 
 // Chiedi colore preferito
+var colorePreferito = prompt( 'Quale è il tuo colore preferito?' );
+console.log( colorePreferito );
+
+// Views sulla pagina
+document.writeln( nomeUtente + cognomeUtente + colorePreferito);
